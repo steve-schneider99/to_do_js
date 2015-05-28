5ac5d9652f9c8e333e1eea3c89dd477f8b128197
Paul Ogawa & Steve Schneider <student@epicodus.com>
2015-05-28 10:17:46 -0700

    Adds revision to clean up some lines of code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 var countUp = function(number, divisor) {
-  var times = Math.floor(number/divisor);
   var list = 1
   var numbers = []
   for (var i = 0; i < Math.floor(number/divisor); i++) {
-    numbers.push(divisor*list)
-    list++
+    numbers.push(divisor*(i+1))
   }
  return numbers
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,10 @@
 describe("countUp", function(){
   it("will take a number and a divisor and increment up", function(){
-  expect(countUp(30, 5)).to.equal([5, 10, 15, 20, 25, 30])
-
+  expect(countUp(30, 5)).to.eql([5, 10, 15, 20, 25, 30])
+  expect(countUp(10, 5)).to.eql([5, 10])
+  expect(countUp(82, 9)).to.eql([9,18,27,36,45,54,63,72,81])
 });
+
+
 
 });
